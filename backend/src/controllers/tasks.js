22ddcf83df6e07580922b5e717dc0266bb3f85e9
Cristian Camilo Bahamon <bahamon2803@gmail.com>
2022-04-16 03:43:02 -0500

    create all functions and routes for task

--- backend/src/controllers/tasks.js
+++ backend/src/controllers/tasks.js
@@ -1,27 +1,58 @@
-import {connect} from '../database';
+import { connect } from '../database';
 
 
-export const getTasks = async (req, res) =>{
+export const getTasks = async (req, res) => {
 	const connection = await connect();
 	const [rows] = await connection.query('SELECT * from tasks')
 	res.json(rows)
 }
-export const getTask = async(req, res) =>{
+
+
+export const getTask = async (req, res) => {
 	const connection = await connect();
-	const [rows] = await connection.query('SELECT * from tasks WHERE id = ?',[
+	const [rows] = await connection.query('SELECT * from tasks WHERE id = ?', [
 		req.params.id
 	])
 	res.json(rows[0])
 }
-export const getTaskCount = (req, res) =>{
-	res.send("Hello World!!")
+
+
+export const getTaskCount = async (req, res) => {
+	const connection = await connect();
+	const [rows] = await connection.query('SELECT count(*) FROM tasks');
+	console.log(rows[0]);
+	res.json(rows[0]["count(*)"]);
 }
-export const saveTask = (req, res) =>{
-	res.send("Hello World!!")
+
+
+export const saveTask = async (req, res) => {
+	const connection = await connect();
+	const [results] = await connection.query('INSERT INTO tasks(title, description) VALUES(?,?)', [
+		req.body.title,
+		req.body.description
+	]);
+	res.json({
+		id: results.insertId,
+		...req.body,
+	})
 }
-export const deleteTask = (req, res) =>{
-	res.send("Hello World!!")
+
+
+export const deleteTask = async (req, res) => {
+	const connection = await connect();
+	 await connection.query('DELETE FROM tasks WHERE id = ?', [
+		req.params.id
+	])
+	res.sendStatus(204)
 }
-export const updateTask = (req, res) =>{
-	res.send("Hello World!!")
+
+
+export const updateTask = async (req, res) => {
+	const connection = await connect();
+	await connection.query('UPDATE tasks SET ? where id = ?',[
+		req.body,
+		req.params.id
+	])
+
+	res.sendStatus(204)
 }
--- backend/src/routes/task.routes.js
+++ backend/src/routes/task.routes.js
@@ -3,17 +3,65 @@ import { deleteTask, getTask, getTaskCount, getTasks, saveTask, updateTask } fro
 
 const router = Router();
 
+
+/** 
+ * @swagger 
+ * /tasks: 
+  *   get:
+ *     tags:
+ *       - Tasks
+ *     description: Get all tasks 
+ */
 router.get('/tasks', getTasks);
 
+/** 
+ * @swagger 
+ * /tasks/count: 
+  *   get:
+ *     tags:
+ *       - Tasks
+ *     description: Get total tasks 
+ */
 router.get('/tasks/count', getTaskCount);
 
+/** 
+ * @swagger 
+ * /tasks/:id: 
+  *   get:
+ *     tags:
+ *       - Tasks
+ *     description: Get task by id
+ */
 router.get('/tasks/:id', getTask)
 
-
+/** 
+ * @swagger 
+ * /tasks: 
+  *   post:
+ *     tags:
+ *       - Tasks
+ *     description: Create a new task
+ */
 router.post('/tasks', saveTask);
 
+/** 
+ * @swagger 
+ * /tasks/:id: 
+  *   delete:
+ *     tags:
+ *       - Tasks
+ *     description: Delete task by id 
+ */
 router.delete('/tasks/:id', deleteTask);
 
+/** 
+ * @swagger 
+ * /tasks/:id: 
+  *   put:
+ *     tags:
+ *       - Tasks
+ *     description: update task by id 
+ */
 router.put('/tasks/:id', updateTask);
 
 
