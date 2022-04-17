c30552c16959a1a1d7db84a640b366c4e9c9dfb5
Cristian Camilo Bahamon <bahamon2803@gmail.com>
2022-04-17 01:06:05 -0500

    fix the bug of config

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,8 +3,8 @@ import {config as dotenv} from 'dotenv';
 dotenv();
 
 export const config = {
-	host: process.env.DB_HOST || 'localhost',
-	user: process.env.DB_USER || 'root',
-	password: process.env.DB_PASSWORD || '1234',
-	database : process.env.DB_DATABASE || 'taskdb' 
+	host: "localhost",
+	user: 'root',
+	password: '1234',
+	database : 'taskdb' 
 }
