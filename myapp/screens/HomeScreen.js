c98a2635f96bb43f66b9738885969b4459dcf64b
Cristian Bahamon <bahamon2803@gmail.com>
2022-04-23 02:50:59 -0500

    Screen of Home

--- myapp/screens/HomeScreen.js
+++ myapp/screens/HomeScreen.js
@@ -1,17 +1,15 @@
-import React,{useEffect} from 'react'
-import { View, Text } from 'react-native'
+import React from 'react'
+import { View  } from 'react-native'
+import Layout from '../components/Layout'
+import TaskList from '../components/TaskList'
 
 const HomeScreen = () => {
 
-useEffect(()=>{
-	console.log('hola');
-})
-
-  return (
-	<View>
-	  <Text>HomeScreen</Text>
-	</View>
-  )
+	return (
+		<Layout>
+			<TaskList />
+		</Layout>
+	)
 }
 
 export default HomeScreen
